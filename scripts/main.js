1831c7897323753dcbe19f280c8d541ab953f2df
Vaibhav <vkona8@gmail.com>
2017-02-22 17:23:15 +0530

    added copied files

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+// Angular code*****************************************************************
+
+// (function(){
+//
+//   var app = angular.module("portApp", []);
+//
+//   app.controller("portCtrl", function(){
+//     var projectData = [
+//       {
+//         name : "random-quote-generator",
+//         url : "https://adred8.github.io/reading-app/",
+//         img : "https://github.com/adred8/reading-app/blob/master/Capture.JPG"
+//         tags : ["HTML", "CSS", "JSON"];
+//       },
+//       {
+//
+//       }
+//     ];
+//
+//   });
+//
+// });
+
+var projectData = [
+      {
+        name : "reading-app",
+        url : "https://adred8.github.io/reading-app/",
+        img : "https://github.com/adred8/reading-app/blob/master/Capture.JPG"
+        tags : ["HTML", "CSS", "JSON"];
+      },
+      {
+        name : "tasks",
+        url : "https://adred8.github.io/tasks/",
+        img : "https://raw.githubusercontent.com/adred8/tasks/master/Capture.JPG"
+        tags : ["HTML", "CSS", "AngularJs"];
+      }
+];
